ec110b4d43ce1fa9c711139aa761f6c35d3e5e3b
Michael Eichenseer <michael.eichenseer@codecentric.de>
2021-05-14 16:34:56 +0200

    Retrieve message that user reacted to.

--- app.py
+++ app.py
@@ -15,21 +15,20 @@ def handle_reaction_added(payload):
     print(payload)
     reaction = payload.get('reaction')
 
+    slack_channel = payload.get('item').get('channel')
+    slack_thread_timestamp = payload.get('item').get('ts')
+
     print("Retrieving content of message")
-    content = "a test content"
+    content = slack_client.retrieve_slack_message(slack_channel, slack_thread_timestamp)
     print("Translating content from <de> to <en>")
     translation = ubersetzer_client.translate(UbersetzerLanguage.GERMAN,
                                               UbersetzerLanguage.ENGLISH,
                                               content)
 
     print("Sending translation back in thread")
-    slack_channel = payload.get('item').get('channel')
-    slack_thread_timestamp = payload.get('item').get('ts')
-    slack_client.reply(
-        channel=slack_channel,
-        thread_timestamp=slack_thread_timestamp,
-        message=translation)
+    slack_client.reply(channel=slack_channel, thread_timestamp=slack_thread_timestamp, message=translation)
 
 
 if __name__ == '__main__':
     app.start(port=3000)
+
