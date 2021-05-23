27954fd8b5894939022fd1246dd26372b3012da3
Michael Eichenseer <michael.eichenseer@codecentric.de>
2021-05-23 23:24:23 +0200

    Add alt text for Slack replies.

--- slack_client.py
+++ slack_client.py
@@ -70,4 +70,5 @@ class SlackClient(object):
     def reply(self, channel: str, thread_timestamp: str, blocks: list):
         _ = self.client.chat_postMessage(channel=channel,
                                          blocks=blocks,
+                                         text="ubersetzer: New translation for a thread you follow.",
                                          thread_ts=thread_timestamp)
